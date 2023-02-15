29eba932bc86ae21e1f5a4495a484c30cfd023f9
paramita <citramulia31@gmail.com>
2023-02-15 09:53:18 +0700

    co success

--- daftar_tamu.php
+++ daftar_tamu.php
@@ -1,12 +1,12 @@
-<?php include 'header.php'?>
-    <div class="card-body">
+<?php include 'header.php' ?>
+<div class="card-body">
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Data Tamu Hari ini [<?= date('d-m-Y')?>]</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Data Tamu Hari ini [<?= date('d-m-Y') ?>]</h6>
         </div>
         <div class="card-body">
             <a href="rekapitulasi.php" class="btn btn-success mb-3"><i class="fa fa-table"></i> Rekapitulasi Tamu</a>
-            
+
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
@@ -32,26 +32,29 @@
                         while ($data = mysqli_fetch_array($show)) {
                         ?>
                             <tr>
-                                <td><?=$no++?></td>
-                                <td><?=$data["tanggal"]?></td>
-                                <td><?=$data["nama"]?></td>
-                                <td><?=$data["asal"]?></td>
-                                <td><?=$data["telepon"]?></td>
-                                <td><?=$data["tujuan"]?></td>
-                                <td><?=$data["keperluan"]?></td>
-                                <td><?=$data["foto"]?></td>
-                                <td><?=$data["time_in"]?></td>
-                                <td><?=$data["time_out"]?></td>
+                                <td><?= $no++ ?></td>
+                                <td><?= $data["tanggal"] ?></td>
+                                <td><?= $data["nama"] ?></td>
+                                <td><?= $data["asal"] ?></td>
+                                <td><?= $data["telepon"] ?></td>
+                                <td><?= $data["tujuan"] ?></td>
+                                <td><?= $data["keperluan"] ?></td>
+                                <td><?= $data["foto"] ?></td>
+                                <td><?= $data["time_in"] ?></td>
+                                <td><?= $data["time_out"] ?></td>
                                 <form action="out.php" method="post">
-                                <td><button class= "btn btn-danger" type="submit">Keluar</button></td>
+                                    <td>
+                                        <input type="hidden" value="<?= $data["id_tamu"] ?>" name="id_tamu">
+                                        <button class="btn btn-danger" type="submit">Keluar</button>
+                                    </td>
                                 </form>
                             </tr>
-                        <?php } ?>    
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
-        </div> 
+        </div>
     </div>
-    </div>
+</div>
 
-<?php include 'footer.php'?>
+<?php include 'footer.php' ?>
--- out.php
+++ out.php
@@ -1,27 +1,15 @@
-<?php 
+<?php
 date_default_timezone_set('asia/jakarta');
 include('koneksi.php');
 
 $id_tamu    = $_POST['id_tamu'];
-$tgl        = $_POST['tanggal'];
-$nama       = $_POST['nama'];
-$asal       = $_POST['asal'];
-$telepon    = $_POST['telepon'];
-$tujuan     = $_POST['tujuan'];
-$keperluan  = $_POST['keperluan'];            
-$time_in    = $_POST['time_in'];
-$foto       = $_POST['foto'];
 $leave      = date('H:i:s');
 
-$keluar = mysqli_query($conn, "UPDATE daftar_tamu SET tanggal='$tgl', nama='$nama', asal='$asal', telepon='$telepon', tujuan='$tujuan', 
-keperluan='$keperluan', time_in='$time_in', foto='$foto', time_out='$leave' WHERE id_tamu='$id_tamu'");
-if($keluar) {
+$keluar = mysqli_query($conn, "UPDATE daftar_tamu SET time_out='$leave' WHERE id_tamu='$id_tamu'");
+if ($keluar) {
     echo "<script>alert('Check Out Berhasil');
         document.location='daftar_tamu.php'</script>";
-}
-else{
+} else {
     echo "<script>alert('Check Out Gagal');
         document.location='daftar_tamu.php'</script>";
 }
-
-?>
